9fe8fed3e6346f82d6ca83952af032c6f31c5807
Amirul Azim <47412407+sovayle@users.noreply.github.com>
2021-11-10 08:01:05 +0800

    Add files via upload

---
+++ webbers.js
@@ -0,0 +1,8 @@
+document.write("hello from javascript.js");
+document.getElementById("demo").innerHTML = "My First JavaScript";
+document.getElementById("demo2").innerHTML = "My 2nd JavaScript";
+
+function myFunction() {
+    document.getElementById("demo").innerHTML = "Paragraph changed.";
+    document.getElementById("demo2").innerHTML = "Paragraph changed again.";
+  }
